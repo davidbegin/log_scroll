a4599d65e354a7018909dab817197a793971ac59
David Begin <davidmichaelbe@gmail.com>
2015-09-14 00:53:25 -0700

    Adds initial docs to Scroll class

--- lib/log_scroll/scroll.rb
+++ lib/log_scroll/scroll.rb
@@ -3,29 +3,44 @@ require "fileutils"
 module LogScroll
   class Scroll
 
+    # @param file_name [String] the name of the file to save log entries to
+    # @param max_size [Integer] the number of entries to save before deleting the oldest
+    #
+    # the name of the file_name passed in will be found or created in the directory
+    # LogScroll is loaded in.
     def initialize(file_name:, max_size:)
       @file_name = file_name
       @max_size  = max_size
-      find_create_history_file!
-      entries
+      find_or_create_history_file!
+      load_entries!
     end
 
+    # saves the string passed in to the file LogScroll is initialized with
+    #
+    # @param log_entry [String] a string of what you would like to save
     def log(log_entry)
       append_log_entry(log_entry)
     end
 
+    # @return [Array] returns every log entry saved
+    #
+    # @example
+    #   ["Created File", "Saved file"]
     def entries
       @entries ||= Array(log_file.each_line.to_a)
     end
 
+    # @return [String] returns the last entry logged with LogScroll
     def newest_entry
       @entries.last
     end
 
+    # @return [String] returns the oldest entry logged with LogScroll
     def oldest_entry
       @entries.first
     end
 
+    # @return [Integer] returns the total number of entries currently saved
     def entry_count
       entries.count
     end
@@ -33,6 +48,8 @@ module LogScroll
     private
 
     attr_reader :file_name, :max_size
+
+    alias_method :load_entries!, :entries
 
     def append_log_entry(log_entry)
       @entries.shift if entry_count == max_size
@@ -51,7 +68,7 @@ module LogScroll
       File.open(file_name, "r+")
     end
 
-    def find_create_history_file!
+    def find_or_create_history_file!
       unless File.exists? file_name
         FileUtils.touch file_name
       end
