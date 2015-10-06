40cb04f7abaaa252d94e8c25ac9ea5d319f5a520
David Begin <davidmichaelbe@gmail.com>
2015-10-05 21:43:27 -0700

    update examples to have to log 2 things

--- examples/sample.rb
+++ examples/sample.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/log_scroll"
 
 p scroll = LogScroll.new(file_name: "test.log", max_size: 4)
-scroll.log("hello1")
+scroll.log("hello my new friend")
+scroll.log("hello my old friend")
 puts "Oldest Entry: " + scroll.oldest_entry.to_s
 puts "Newest Entry: " + scroll.newest_entry.to_s
